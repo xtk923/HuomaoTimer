b469c4977c7c54addb42e7700854a17287a466b6
Xiong Tiankai <xiongtk@gmail.com>
2019-08-03 16:19:37 +0800

    Allow pressing Enter to reset progress bar

--- bin/refresh_timer.jl
+++ bin/refresh_timer.jl
@@ -2,6 +2,5 @@ using Pkg
 Pkg.activate(joinpath(@__DIR__, ".."))
 
 using HuomaoTimer
-using Dates
 
-HuomaoTimer.start_timer(endTime = DateTime(today()) + Hour(13))
+HuomaoTimer.start_timer()
--- src/HuomaoTimer.jl
+++ src/HuomaoTimer.jl
@@ -3,20 +3,42 @@ module HuomaoTimer
 using ProgressMeter
 using Dates
 
-defaultTime = DateTime(today()) + Hour(18) + Minute(30)
+export start_timer
+
+if Dates.isfriday(today())
+    defaultTime = DateTime(today()) + Hour(18)
+else
+    defaultTime = DateTime(today()) + Hour(18) + Minute(30)
+end
 
 
 
 function start_timer(;endTime::DateTime = defaultTime)
 
     n = Dates.value(endTime - now())
+    prog = Progress(n, 0.1, "", 25)
 
-    prog = Progress(n)
+    t_reset = @task readavailable(stdin)
+    schedule(t_reset)
+    yield()
 
     while now() <= endTime
+        sleep(0.1)
         c = n - Dates.value(endTime - now())
+        if istaskdone(t_reset)
+            n = Dates.value(endTime - now())
+            prog = Progress(n, 0.1, "", 25)
+
+            t_reset = @task readavailable(stdin)
+            schedule(t_reset)
+            yield()
+	    end
+
         update!(prog, c)
-    end
+
+	end
+    finish!(prog)
+    println("\n\n Unleash Huomao! |･ω･｀)")
 end
 
 
